1e32532d3b5b01bf9ce4f0993655ad071e98e298
Hene Chen <henecastro@gmail.com>
2022-06-10 13:40:39 -0300

    For decrescente, enquanto o counter for menor ou igual a zero, vai descontar e subtrair 2 do counter

---
+++ controle/For2.java
@@ -0,0 +1,12 @@
+package controle;
+
+public class For2 {
+
+    public static void main(String[] args) {
+
+        //for() decrescente
+        for(int counter = 10; counter >= 0; counter -= 2) {
+            System.out.printf("Contador = %d\n", counter);
+        }
+    }
+}
